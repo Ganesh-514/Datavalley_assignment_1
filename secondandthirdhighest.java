daa1c779729c7c1856b090ba654b7f36dfe97498
ganesh <113087407+Ganesh-514@users.noreply.github.com>
2024-03-13 16:10:16 +0530

    Update secondandthirdhighest.java

--- secondandthirdhighest.java
+++ secondandthirdhighest.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {10, 51, 53, 39, 47, 44, 21}; 
         Arrays.sort(array);
-        System.out.println(array[1]+" "+array[2]);
+        System.out.println(array[1]+array[2]);
     }        
 }
